36e95ae999f488f569af0e3ef02a61fc3f322eec
lwinnicki <lukasz.winnicki@refurbed.com>
2024-07-01 08:37:03 +0200

    alacritty config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 ###### Variables
 
-dir=~/dotfiles-ubuntu                    # dotfiles directory
+dir=~/dotfiles-ubuntu # dotfiles directory
 files=".bash_profile .vimrc .ideavimrc .aliases .functions .paths .exports .tmux.conf .zshrc z.sh"
 
 ##########
@@ -10,10 +10,14 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Creating symlink to $file in home directory."
-    ln -s -f $dir/$file ~/$file
+	echo "Creating symlink to $file in home directory."
+	ln -s -f $dir/$file ~/$file
 done
+
+# alacritty config
+mkdir -p ~/.config/alacritty
+ln -s -f $dir/alacritty.toml ~/.config/alacritty/alacritty.toml
 
 source ~/.bash_profile
