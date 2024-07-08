c01e039c2660f2a42b399dbe15cbba92655a4efa
lwinnicki <lukasz.winnicki@refurbed.com>
2024-07-08 12:24:50 +0200

    change weztermi color - gruvbox

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "catppuccin-macchiato"
+--config.color_scheme = "catppuccin-macchiato"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font = wezterm.font("JetBrains Mono")
 -- config.window_background_opacity = 0.9
 
