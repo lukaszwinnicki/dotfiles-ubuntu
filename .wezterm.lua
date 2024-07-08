f0749e8b28d329dd0d4ae5db1635da7d04fee792
lwinnicki <lukasz.winnicki@refurbed.com>
2024-07-08 12:33:40 +0200

    wezterm - hide tabs if only one

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,5 +6,6 @@ local config = wezterm.config_builder()
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font = wezterm.font("JetBrains Mono")
 config.window_background_opacity = 0.9
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
