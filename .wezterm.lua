b61eeb3017519d438c2d73a3b80d10da02bf7453
lwinnicki <lukasz.winnicki@refurbed.com>
2024-07-08 12:27:44 +0200

    wezterm - transparrent

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,6 +5,6 @@ local config = wezterm.config_builder()
 --config.color_scheme = "catppuccin-macchiato"
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font = wezterm.font("JetBrains Mono")
--- config.window_background_opacity = 0.9
+config.window_background_opacity = 0.9
 
 return config
