c491ba3a50ac0a95daae0d9a722c96a790b52cbd
lwinnicki <lukasz.winnicki@refurbed.com>
2024-07-18 09:44:31 +0200

    use default font in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,12 +4,12 @@ local config = wezterm.config_builder()
 
 --config.color_scheme = "catppuccin-macchiato"
 config.color_scheme = "Gruvbox Dark (Gogh)"
-config.font = wezterm.font("JetBrains Mono")
-config.font = wezterm.font_with_fallback({
-	"JetBrains Mono",
-	"Fira Code",
-	"DengXian",
-})
+-- config.font = wezterm.font("JetBrains Mono")
+-- config.font = wezterm.font_with_fallback({
+-- 	"JetBrains Mono",
+-- 	"Fira Code",
+-- 	"DengXian",
+-- })
 config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
 
